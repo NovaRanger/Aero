9f55ba92dbb233158532a1840cb5896a452f28ba
LizGamezz <stormygrl321@aim.com>
2024-04-17 23:09:47 -0400

    Figured out the score and brought it to the end screen as well

--- app/src/main/java/com/group/project/GameoverActivity.java
+++ app/src/main/java/com/group/project/GameoverActivity.java
@@ -2,22 +2,32 @@ package com.group.project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 // LIZ - GAMEOVER / END SCREEN WITH SCORE
 
-public class GameoverActivity extends AppCompatActivity{
+public class GameoverActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gameover_activity);
+
+        // Bringing the score to this screen
+        Intent intent = getIntent();
+        int score = intent.getIntExtra("SCORE", 0);
+
+        TextView scoreText = findViewById(R.id.scoreText);
+        scoreText.setText(String.valueOf(score));
     }
-    public void onReplayClick(View view){
+
+    public void onReplayClick(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-    public void onMenuClick(View view){
+
+    public void onMenuClick(View view) {
         Intent intent = new Intent(this, HelpActivity.class);
         startActivity(intent);
     }
