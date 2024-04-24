04b6762b4e47ddece33ece6bcac82ff457b462b5
LizGamezz <stormygrl321@aim.com>
2024-04-24 11:07:43 -0400

    Made it to where the layout color changes with the button click

--- app/src/main/java/com/group/project/ColorActivity.java
+++ app/src/main/java/com/group/project/ColorActivity.java
@@ -2,18 +2,50 @@ package com.group.project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ColorActivity extends AppCompatActivity {
 
-
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.color_activity);
 
+
+        ImageButton defaultButton = findViewById(R.id.defaultButton);
+        defaultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.d_pad_layout);
+            }
+        });
+
+        ImageButton yellowButton = findViewById(R.id.YellowButton);
+        yellowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.d_pad_layout_yellow);
+            }
+        });
+
+        ImageButton pinkButton = findViewById(R.id.PinkButton);
+        pinkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.d_pad_layout_pink);
+            }
+        });
+
+        ImageButton greenButton = findViewById(R.id.GreenButton);
+        greenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.d_pad_layout_green);
+            }
+        });
     }
 }
 
